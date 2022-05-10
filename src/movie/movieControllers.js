4ca0288982f43c4bafcd7b7485d7888564ada2c3
Sayam Ajmal <itssanesayam@gmail.com>
2022-05-10 15:21:48 +0100

    delete CRUD working

--- src/movie/movieControllers.js
+++ src/movie/movieControllers.js
@@ -19,3 +19,13 @@ exports.listMovies = async (req, res) => {
         res.status(500).send({ error: error.message })
     }
 };
+
+exports.deleteMovie = async (req, res) => {
+    try {
+        const newMovie = await Movie.deleteOne(req.body);
+        res.status(200).send({ movie: newMovie });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message });
+    }
+}
