009ecbdd12912672aa5c05f37b6f93cfb9657e93
Sayam Ajmal <itssanesayam@gmail.com>
2022-05-11 15:06:17 +0100

    Delete CRUD working for User

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,6 +2,8 @@ const User = require("./userModel");
 
 // create a function to add a new user to our database
 // using User schema to add JSON data for sign up
+
+//add user
 exports.addUser = async (req, res) => {
   try {
     const newUser = await User.create(req.body);
@@ -14,6 +16,8 @@ exports.addUser = async (req, res) => {
   }
 };
 
+
+//list user
 exports.listUsers = async (req, res) => {
     try {
         const allUsers = await User.find({});
@@ -23,6 +27,18 @@ exports.listUsers = async (req, res) => {
         res.status(500).send({ error: error.message })
     } 
 };
+
+//delete user
+exports.deleteUser = async (req, res) => {
+    try {
+      const delUser = await User.deleteOne(req.body);
+      res.status(200).send({ user: delUser });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message });
+    }
+  };
+
 
 
 
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,6 +1,6 @@
 // Require functions and libraries that we need for this file
 const { Router } = require("express");
-const { addUser, listUsers} = require("./userController");
+const { addUser, listUsers, deleteUser} = require("./userController");
 const { hashPass } = require("../middleware/index");
 const userRouter = Router();
 
@@ -8,5 +8,6 @@ const userRouter = Router();
 // run the function once the endpoint is hit.
 userRouter.post("/user",hashPass, addUser, ); //POST
 userRouter.get("/user", listUsers); //GET
+userRouter.delete("/user", deleteUser); //DELETE
 
 module.exports = userRouter;
